9a42dff989cf58aeef47e0dc11baa1d556e297fa
unknown <abusiddick84@gmail.com>
2024-04-15 22:29:38 +0600

    Navbar and login button design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
- 
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h1>This is home page.</h1>
+            <Footer></Footer>
         </div>
     );
 };
